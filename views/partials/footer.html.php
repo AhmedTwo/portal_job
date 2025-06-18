c0831be591620eae83143b6289d76ce7ca2a5ec3
Mayalabeille95 <seghiriahmed9@gmail.com>
2025-06-18 11:42:04 +0200

    2025 footer

--- views/partials/footer.html.php
+++ views/partials/footer.html.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark text-white text-center py-3 mt-5">
-    <p class="mb-0">&copy; 2000 - Tous droits réservés</p>
+    <p class="mb-0">&copy; 2025 - Tous droits réservés</p>
 </footer>
 </body>
 
